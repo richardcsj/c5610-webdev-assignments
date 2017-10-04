960805b0c27fb311002b71cf310f7f9cbdca7f35
Richard Chun <richard.chun91@gmail.com>
2017-10-04 18:57:33 +0000

    ameliorating login funtionnality

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit,ViewChild } from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {UserService} from "../../../services/user.service.client";
 
 @Component({
   selector: 'app-login',
@@ -10,18 +11,26 @@ export class LoginComponent implements OnInit {
 
   @ViewChild('f') loginForm: NgForm;
   //properties
-  username: String;
-  password: String;
+  username: string;
+  password: string;
   errorFlag: boolean;
   errorMsg = 'Invalid username or password !';
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit() { }
   login() {
+      //hide error msg if already there
+      this.errorFlag = false;
       // fetching data from loginForm
       this.username = this.loginForm.value.username;
       this.password = this.loginForm.value.password;
-      console.log(this.username,this.password);
+      let user = this.userService.findUserByCredentials(this.username,this.password);
+      if(user==undefined){
+        this.errorFlag = true;
+      }else{
+        //need to redirect to profile
+
+      }
     }
 
 }
