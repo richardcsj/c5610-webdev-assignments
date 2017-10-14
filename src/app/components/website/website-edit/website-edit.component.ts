a88379b3184a1f995e0d5a3ddabb68070810adb2
richardcsj <richard.chun91@gmail.com>
2017-10-15 00:29:00 +0100

    margin added to the top and pages are dynamic in website-edit

--- src/app/components/website/website-edit/website-edit.component.ts
+++ src/app/components/website/website-edit/website-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {WebsiteService} from "../../../services/website.service.client";
+import {PageService} from "../../../services/page.service.client";
 
 @Component({
   selector: 'app-website-edit',
@@ -9,24 +10,26 @@ import {WebsiteService} from "../../../services/website.service.client";
 })
 export class WebsiteEditComponent implements OnInit {
 
-	userId:string;
-	website:any;
-	websiteId:string;
-	websiteName:string;
-	description:string;
-  constructor(private websiteService: WebsiteService, private activatedRoute: ActivatedRoute) { }
+  userId:string;
+  website:any;
+  websiteId:string;
+  websiteName:string;
+  description:string;
+  pagesForWebsite:any[]=[{}];
+  constructor(private websiteService: WebsiteService,private pageService: PageService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-  	this.activatedRoute.params
-	.subscribe(
-		(params: any) => {
-		this.userId = params['userId'];
-		this.websiteId = params['wid'];
-		} 
-	);
-	this.website = this.websiteService.findWebsiteById(this.websiteId);
-	this.websiteName  = this.website['name'];
-	this.description = this.website['description'];
+    this.activatedRoute.params
+  .subscribe(
+    (params: any) => {
+    this.userId = params['userId'];
+    this.websiteId = params['wid'];
+    } 
+  );
+  this.website = this.websiteService.findWebsiteById(this.websiteId);
+  this.pagesForWebsite = this.pageService.findPageByWebsiteId(this.websiteId);
+  this.websiteName  = this.website['name'];
+  this.description = this.website['description'];
   }
 
 }
