ca6062abbd3c1c01d34bc849d39b487362f7d3c0
richardcsj <richard.chun91@gmail.com>
2017-10-18 18:04:04 +0100

    user service implemented server side

---
+++ assignment/services/user.service.server.js
@@ -0,0 +1,90 @@
+module.exports= function(app){
+	users = [
+    {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder"},
+    {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley"},
+    {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia"},
+    {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi"}];
+
+    api = {
+    'createUser': createUser,
+    'findUser': findUser,
+    'findUserById':findUserById,
+    'updateUser': updateUser,
+    'deleteUser': deleteUser
+  };
+
+	app.post('/api/user',api.createUser);
+	app.get('/api/user',api.findUser);
+	app.get('/api/user/:userId',api.findUserById);
+	app.put('/api/user/:userId',api.updateUser);
+	app.delete('/api/user/:userId',api.deleteUser);
+
+	function createUser(req,res){
+		var user = req.body.user;
+		if(user!=undefined){
+			user._id = ""+Math.floor(Math.random()*900) + 100;
+			users.push(user);
+			res.send(user);
+		}
+	}
+	
+	function findUser(req,res){
+		var username = req.query.username;
+		var password = req.query.password;
+		var user;
+		if(password==undefined){
+			user = findUserByUsername(username);
+		}else{
+			user = findUserByCredentials(username,password);
+		}
+		console.log(user);
+		res.send(user);
+	}
+
+	var findUserByUsername = function(username) {
+	    for (var x = 0; x < users.length; x++) {
+	      if (users[x].username === username) {
+	        return users[x];
+	      }
+	    }
+  	}
+
+  	var findUserByCredentials = function(username, password) {
+	    for (var x = 0; x < users.length; x++) {
+	      if (users[x].username === username && users[x].password === password) {
+	        return users[x];
+	      }
+	    }
+  	}
+
+  	function findUserById(req,res) {
+  		var userId = req.params.userId;
+	    for (var x = 0; x < users.length; x++) {
+	      if (users[x]._id === userId) {
+	         res.send(users[x]);
+	      }
+	    }
+  	}
+
+  	function updateUser(req,res){
+  		var userId = req.params.userId;
+  		var user = req.body.user;
+  		for (var x = 0; x < users.length; x++) {
+      		if (users[x]._id === userId) {
+        		users[x] = user;
+     		}
+    	}
+  	}
+
+  	function deleteUser(req,res){
+  		var userId = req.params.userId;
+  		for (var x = 0; x < users.length; x++) {
+      		if (users[x]._id === userId) {
+        	var index = users.indexOf(users[x], 0);
+        		if (index > -1) {
+           			users.splice(index, 1);
+        		}
+      		}
+    	}
+  	}
+}
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // Point static path to dist -- For building -- REMOVE
-app.use(express.static(path.join(__dirname, 'dist')));
+//app.use(express.static(path.join(__dirname, 'dist')));
 
 
 
@@ -33,23 +33,18 @@ app.use(function(req, res, next) {
 
 
 const port = process.env.PORT || '3100';
-app.set('port', port);
-
 
 // Create HTTP server
-const server = http.createServer(app);
+/*const server = http.createServer(app);
 
 var serverSide = require("./server/test-mongodb/app");
 serverSide(app);
 
 
-
+*/
 // For Build: Catch all other routes and return the index file -- BUILDING
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
-
-
-server.listen( port , () => console.log('Running'));
-
-
+require("./assignment/app.js")(app);
+app.listen( port , '127.0.0.1');
