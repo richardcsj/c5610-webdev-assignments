0db036eb7e774f1d736e0b8bd962e0d03073f0a8
richardcsj <richard.chun91@gmail.com>
2017-10-31 21:46:11 +0000

    user model done and refactored

--- assignment/model/user/user.model.server.js
+++ assignment/model/user/user.model.server.js
@@ -1,28 +1,49 @@
 module.exports = function(app,mongoose){
 
 	var UserSchema = require("./user.schema.server.js")(app,mongoose),
-	UserModel = mongoose.model("UserModel", UserSchema);
+	UserModel = mongoose.model("UserModel", UserSchema),
+	api = {
+		"createUser":createUser,
+		"findUserById":findUserById,
+		"findUserByUsername":findUserByUsername,
+		"findUserByCredentials":findUserByCredentials,
+		"updateUser":updateUser,
+		"deleteUser":deleteUser
+	}
 
-	return {
-		createUser:function(user){
-			user._id = Math.floor(Math.random()*900) + 100;
-			user.dateCreated = new Date();
-			return UserModel
-				.create(user);
-				
-		},
+	return api;
 
-		findUserById:function(userId){
-			return UserModel
-				.findById(userId);
-		},
-		findUserByUsername:function(username){
-			return UserModel
-				.findOne({username:username});
-		},
-		findUserByCredentials:function(username, password){
-			return UserModel
-				.findOne({username:username,password:password});
-		}
+	
+	function createUser(user){
+		user._id = Math.floor(Math.random()*900) + 100;
+		user.dateCreated = new Date();
+		return UserModel
+			.create(user);
+			
+	}
+
+	function findUserById(userId){
+		return UserModel
+			.findById(userId);
+	}
+	
+	function findUserByUsername(username){
+		return UserModel
+			.findOne({username:username});
+	}
+	
+	function findUserByCredentials(username, password){
+		return UserModel
+			.findOne({username:username,password:password});
+	}
+	
+	function updateUser(userId,user){
+		return UserModel
+			.findOneAndUpdate({_id:userId},user);
+	}
+
+	function deleteUser(userId){
+		return UserModel
+			.remove({_id:userId});
 	}
 }
--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -45,7 +45,6 @@ module.exports= function(app,userModel){
 			userModel.findUserByCredentials(username,password)
 				.then(
 					function(user){
-						console.log(user);
 						res.send(user);
 					},
 					function(error){
@@ -57,8 +56,7 @@ module.exports= function(app,userModel){
 
   	function findUserById(req,res) {
   		var userId = req.params.userId;
-  		userModel.
-  			findUserById(userId)
+  		userModel.findUserById(userId)
 	  		.then(
 					function(user){
 						res.send(user);
@@ -75,32 +73,29 @@ module.exports= function(app,userModel){
   	function updateUser(req,res){
   		var userId = req.params.userId;
   		var user = req.body.user;
-  		var updated = false;
-  		for (var x = 0; x < users.length; x++) {
-      		if (users[x]._id === userId) {
-        		users[x] = user;
-        		updated = true;
-        		res.send({updated:true});
-     		}
-    	}
-    	if(!updated)
-    		res.status(404).send("couldn't find user for userId");
+  		userModel.updateUser(userId,user)
+  			.then(
+  					function(user){
+  						res.send(user);
+  					},
+  					function(error){
+  						console.log(error);
+  						res.status(404).send("couldn't find user for userId");
+  					}
+  				); 		
   	}
 
   	function deleteUser(req,res){
   		var userId = req.params.userId;
-  		var deleted = false;
-  		for (var x = 0; x < users.length; x++) {
-      		if (users[x]._id === userId) {
-        	var index = users.indexOf(users[x], 0);
-        		if (index > -1) {
-           			users.splice(index, 1);
-           			deleted = true;
-           			res.send({deleted:true});
-        		}
-      		}
-    	}
-    	if(!deleted)
-    		res.status(404).send("Couldn't find user for userId");
+  		userModel.deleteUser(userId)
+  			.then(
+  					function(result){
+  						res.send({deleted:true});
+  					},
+  					function(error){
+  						console.log(error);
+  						res.status(404).send("Couldn't find user for userId");
+  					}
+  				);		
   	}
 }
